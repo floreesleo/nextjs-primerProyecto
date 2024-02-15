e57540c0ba33b2d2552df79112f000b6794d06dc
floreesleo <florees.leo@gmail.com>
2024-02-15 12:56:27 -0600

    build: :alien: Llamado de API
    Llamado de una API de usuarios por medio de una función fetch() y mostrado por medio de un mapeado en una lista básica

--- app/page.js
+++ app/page.js
@@ -1,5 +1,23 @@
-function HomePage() {
-  return <div>Home</div>;
+async function fetchUsers() {
+  const res = await fetch("https://reqres.in/api/users");
+  const data = await res.json();
+  return data.data;
 }
 
-export default page;
+export default async function HomePage() {
+  const users = await fetchUsers();
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          <div>
+            <h5>
+              {user.id} | {user.first_name} {user.last_name}
+            </h5>
+            <p>Email: {user.email}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
