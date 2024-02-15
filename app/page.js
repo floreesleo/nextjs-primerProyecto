492dec26b6c5d228f4bf15054235c97929729deb
floreesleo <florees.leo@gmail.com>
2024-02-15 13:14:05 -0600

    style: :art: Estilos de la aplicación
    Estilos en la aplicación,estilo de card en listado de usuarios con fotografia y email

--- app/page.js
+++ app/page.js
@@ -9,13 +9,19 @@ export default async function HomePage() {
   return (
     <ul>
       {users.map((user) => (
-        <li key={user.id}>
+        <li
+          key={user.id}
+          className="bg-zinc-200 mb-2 p-4 rounded-md text-black flex justify-between"
+        >
           <div>
-            <h5>
+            <h5 className="font-bold">
               {user.id} | {user.first_name} {user.last_name}
             </h5>
-            <p>Email: {user.email}</p>
+            <p>
+              Email: <span className="text-blue-800">{user.email}</span>
+            </p>
           </div>
+          <img src={user.avatar} className="rounded-full w-16" />
         </li>
       ))}
     </ul>
